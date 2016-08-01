7e96ce6adc9f6f9479517514312638836ee05249
Mohamed Gupta <philjallman+fti@gmail.com>
2016-08-01 23:33:39 +0000

    Fixed

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 import twitter, twitter_config
-import requests, json, re
-import untangle
+import untangle, pickle
 import youtube_search
 from os import path
 from datetime import datetime
@@ -9,11 +8,11 @@ from datetime import datetime
 
 cred = next(acct for acct in twitter_config.accounts if acct['username'] == 
             'UYDMusic')
-last_epfile = '/home/git/pi/uydmusic/last_ep.p'
+last_epfile = '/home/pi/git/uydmusic/last_ep.p'
 
 def tweet_uyd(api):
     if path.isfile(last_epfile):
-        last_ep = pickle.load(last_epfile)
+        last_ep = pickle.load(open(last_epfile, 'r'))
     else:
         last_ep = datetime.strptime('Mon, 01 Feb 2016 19:51:00 +0000', '%a, %d %b %Y %H:%M:%S +0000')
 
@@ -27,6 +26,13 @@ def tweet_uyd(api):
         pubdate = datetime.strptime(pubdate, '%a, %d %b %Y %H:%M:%S +0000')
         if pubdate > last_ep:
             new_eps.append(ep)
+            last_ep = pubdate
+
+    if new_eps == []:
+        print 'No new eps'
+        return
+
+    pickle.dump(last_ep, open(last_epfile, 'w'))
     for ep in reversed(new_eps):
         desc = ep.description.cdata
         #words = re.findall(r'[\w]+', desc)
@@ -36,11 +42,10 @@ def tweet_uyd(api):
             intro = ' '.join(words[words.index('intro:')+1:words.index('outro:')-1])
             intro_url = youtube_search.search(intro)
             api.PostUpdate('{0} intro: {1} {2}'.format(ep.title.cdata, intro, intro_url))
-            print '{0} intro: {1} {2}'.format(ep.title.cdata, intro, intro_url)
             outro = ' '.join(words[words.index('outro:')+1:words.index('http://uhhyeahdude.com')])
             outro_url = youtube_search.search(outro)
             api.PostUpdate('{0} outro: {1} {2}'.format(ep.title.cdata, outro, outro_url))
-            print '{0} outro: {1} {2}'.format(ep.title.cdata, outro, outro_url)
+
             
 if __name__ == '__main__':
     try:
